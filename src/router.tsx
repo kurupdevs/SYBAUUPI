28b58c78c1c44bc98a01149400fa45deab951cb7
Kurup <160393536+kurupdevs@users.noreply.github.com>
2026-08-11 19:24:05 +0530

    chore: dep

--- src/router.tsx
+++ src/router.tsx
@@ -1,16 +1,4 @@
-import { QueryClient } from "@tanstack/react-query";
-import { createRouter } from "@tanstack/react-router";
-import { routeTree } from "./routeTree.gen";
-
-export const getRouter = () => {
-  const queryClient = new QueryClient();
-
-  const router = createRouter({
-    routeTree,
-    context: { queryClient },
-    scrollRestoration: true,
-    defaultPreloadStaleTime: 0,
-  });
-
-  return router;
-};
+import { createRouter } from "@tanstack/react-router"
+import { routeTree } from "./routeTree.gen"
+export const router = createRouter({ routeTree })
+declare module "@tanstack/react-router"{interface Register{router:typeof router}}
