5a9a0858428469bb9cae2e93b7acb2566b671192
Kurup <160393536+kurupdevs@users.noreply.github.com>
2026-08-11 19:20:35 +0530

    refactor: clean up redundant logic

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,3 +1,6 @@
-import {type ClassValue,clsx}from"clsx"
-import {twMerge}from"tailwind-merge"
-export function cn(...i:ClassValue[]){return twMerge(clsx(i))}
+import { type ClassValue, clsx } from "clsx"
+import { twMerge } from "tailwind-merge"
+
+export function cn(...inputs: ClassValue[]): string {
+  return twMerge(clsx(inputs))
+}
